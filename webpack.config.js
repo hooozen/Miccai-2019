0b32d585deb74fbdcf18fe2b6dab80175cbeba45
李浩然 <lhr@servyou.com.cn>
2018-12-24 16:59:46 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+function getAbPath(dir) {
+  return path.resolve(__dirname, dir);
+}
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: getAbPath('./main.js'),
+    index: getAbPath('./src/index/index.js'),
+  },
+  devServer: {
+    port: 7777,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    }, {
+      test: /\.(png|jpge?|gif)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      }],
+    }],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Miccai',
+      filename: 'index.html',
+      template: getAbPath('./src/pages/index.html'),
+      inject: 'index',
+    }),
+  ],
+}
